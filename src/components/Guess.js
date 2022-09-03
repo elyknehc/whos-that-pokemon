9687e666eea5ed3d86d20771615435c393d9cbc5
elyknehc <kchen727@uwo.ca>
2022-09-03 17:16:15 -0400

    first commit, basic api call added

--- src/components/Guess.js
+++ src/components/Guess.js
@@ -1,13 +1,23 @@
 import React from "react";
 
 const Guess = ({ name, generatePokemon }) => {
+	//Needs to update if the user types in the right answer
+	const [correctGuess, setCorrectGuess] = ["hi"];
 	return (
 		<div>
 			<button className="btn" onClick={generatePokemon}>
-				{" "}
-				Reset{" "}
+				Reset
 			</button>
-			<input className="input" placeholder="Make a Guess!" />
+			<input
+				type="text"
+				className="input"
+				placeholder="Make a Guess!"
+				onChange={(e) =>
+					e.target.value === name ? setCorrectGuess("Correct!") : null
+				}
+			/>
+			<h1> {correctGuess}</h1>
+
 			<h1> {name}</h1>
 		</div>
 	);
